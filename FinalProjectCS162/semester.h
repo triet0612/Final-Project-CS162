4bba54a6c113dbd8a9b818a77676ec75dbef566f
triet0612 <75552835+triet0612@users.noreply.github.com>
2022-03-17 13:27:54 +0700

    Update semester.h

--- FinalProjectCS162/semester.h
+++ FinalProjectCS162/semester.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include "Course_controller.h"
 using namespace std;
 
-class semester
+class semester : public Course_controller
 {
 public:
 	string semester_name;
