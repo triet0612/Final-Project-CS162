bd0ae9e7bec0d50e857b0b934b9fa523390fc8b4
htamlive <nghichmaychoi@gmail.com>
2022-03-21 05:48:18 +0700

    testing Stuff

--- FinalProjectCS162/SinglyLinkedList.h
+++ FinalProjectCS162/SinglyLinkedList.h
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <iostream>
 using namespace std;
-
+#define sll SinglyLinkedList
 template <class T>
 class SinglyNode {
 public:
@@ -136,7 +136,6 @@ public:
         while (curr != nullptr) {
             this->push_back(curr->data);
             curr = curr->next;
-            this->sz++;
         }
     };
 
