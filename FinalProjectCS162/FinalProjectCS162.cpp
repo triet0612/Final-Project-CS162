7dd6ec819f34051241faa631907900b906b55fc9
htamlive <nghichmaychoi@gmail.com>
2022-03-21 08:28:36 +0700

    change location of Data Folder

--- FinalProjectCS162/FinalProjectCS162.cpp
+++ FinalProjectCS162/FinalProjectCS162.cpp
@@ -1,6 +1,6 @@
-#include <string.h>
+#include <string>
 #include <iostream>
-
+#include <fstream>
 //#include "CourseSystem.h"
 //#include "SchoolYear.h"
 //#include "SinglyLinkedList.h"
@@ -13,6 +13,12 @@ int main()
 {
 	//CourseSystem courseSystem;
 	//courseSystem.startProgram();
+	ifstream ifs("input.txt");
+	int n;
+	ifs >> n;
+	cout << n;
+	
 	cout << "Hello World";
-	system("cls");
+	//system("cls");
+	return 0;
 }
