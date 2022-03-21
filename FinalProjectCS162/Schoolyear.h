8fb475c399a78b773c6d29342a0e839775677288
triet0612 <75552835+triet0612@users.noreply.github.com>
2022-03-21 11:07:24 +0700

    Semester, school year update

---
+++ FinalProjectCS162/Schoolyear.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "Semester_controller.h"
+
+class Schoolyear {
+	string this_year;
+};
--- FinalProjectCS162/Semester_controller.h
+++ FinalProjectCS162/Semester_controller.h
@@ -4,19 +4,48 @@
 #include <string.h>
 #include "semester.h"
 #include "SinglyLinkedList.h"
+#include "Date.h"
 using namespace std;
 
 class Semester_controller : public semester
 {
 public: 
-	void create_semester(string yearname) {
-		string n;
-		cout << "Please enter the semester name: [s1] [s2] [s3]" << endl;
-		cin >> n;
-		semester_name = n;
-		string path = "/" + yearname + "/" + n;
-		//int a = mkdir(path.c_str());
+	void create_semester() {
+		cout << "Choose the year to add the semester: " << endl;
+		cin >> yearName;
+		ifstream fin;
+		fin.open('/' + yearName + "/semesters.txt");
+		SinglyLinkedList<string> lst;
+		while (fin.eof()) {
+			string temp;
+			fin >> temp;
+			lst.push_back(temp);
+		}
+		cout << "Current created semesters in year: " << yearName << ": ";
+		for (auto i : lst) {
+			cout << i << " ";
+		}
+		cout << endl << "Input the semester you want to add: " << endl;
+		cin >> semester_name;
+		string path = '/' + yearName + '/' + semesterName;
+		int a = mkdir(path.c_str());
 		return;
 	}
+	void getSemester(int num) {
+		semesterName = 's'+num;
+	}
+	Date createCourseReg() {
+		Date d;
+		d.defaultInputDate();
+		return d;
+	}
+	void viewCourse() {
+		ifstream fin;
+		fin.open('/' + yearName + '/' + semesterName + "Courses.csv");
+		string temp;
+		while (getline(fin, temp, ',')) {
+			getline(fin, temp, ',');
+			cout << temp << " ";
+		}
+	}
 };
-
