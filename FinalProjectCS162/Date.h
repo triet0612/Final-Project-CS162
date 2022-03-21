640cc9f3b5bb5df95ad585ccc59d9a39165c080a
htamlive <nghichmaychoi@gmail.com>
2022-03-21 11:12:24 +0700

    Add date parse from string

--- FinalProjectCS162/Date.h
+++ FinalProjectCS162/Date.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <time.h>
 #include <iostream>
+#include <string>
 
 using namespace std;
 class Date {
@@ -41,6 +42,31 @@ public:
     friend bool operator>=(const Date& d1, const Date& d2) ;
 
     friend bool operator!=(const Date& d1, const Date& d2);
+
+    Date& operator=(const string& s) {
+        int day = 0;
+        int month = 0;
+        int year = 0;
+        int i = 0;
+        while (s[i] != '/') {
+            day = day * 10 + (s[i] - '0');
+            i++;
+        }
+        i++;
+        while (s[i] != '/') {
+            month = month * 10 + (s[i] - '0');
+            i++;
+        }
+        i++;
+        while (i < s.length()) {
+            year = year * 10 + (s[i] - '0');
+            i++;
+        }
+        this->day = day;
+        this->month = month;
+        this->year = year;
+        return *this;
+    };
 
 };
 
