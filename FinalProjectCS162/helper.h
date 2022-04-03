ab9f3eb7c60d83686fd813ec7e9b2357fbad0bf7
htamlive <nghichmaychoi@gmail.com>
2022-04-03 08:44:40 +0700

    add string doubleToStr(double x, int precision) function

--- FinalProjectCS162/helper.h
+++ FinalProjectCS162/helper.h
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <string>
 #include <string.h>
+#include <sstream>
 
 using namespace std;
 
@@ -10,10 +11,24 @@ using namespace std;
 #include "Date.h"
 
 int getNumberOfDigits(long long x);
+
 void adjustString(string& s);
+
 bool checkLeapYear(const int year);
+
 bool checkValidDate(const int day, const int month, const int year);
+
 bool readInteger(int &result);
+
 bool checkIntersection(pair<int, int> a, pair<int, int> b);
+
 int getMinutes(const string& t);
+
 void printDates(ofstream& foutput, const Date& date, const string& s);
+
+string doubleToStr(double x, int precision) {
+	stringstream ss;
+	ss << std::fixed;
+	ss << std::setprecision(precision) << x;
+	return ss.str();
+}
