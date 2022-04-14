c1d4a48f5d322981852134c5e72b50ef15f3cc0d
unknown <nguyen31hoang08minh2003@gmail.com>
2022-04-14 08:10:25 +0700

    Move functions in Background Controller

--- FinalProjectCS162/BackgroundController.cpp
+++ FinalProjectCS162/BackgroundController.cpp
@@ -1,1 +1,50 @@
 #include "BackgroundController.h"
+
+void BackgroundController::drawPicFromPath(const string& path, int x, int y, int color) {
+	ifstream ifs(path);
+	if (ifs.is_open()) {
+		string tmp;
+		int row = 0;
+		ConsoleGraphics::getInstance().color(color);
+		while (!ifs.eof()) {
+			getline(ifs, tmp);
+			if (tmp == "") break;
+			ConsoleGraphics::getInstance().gotoXY(x, y + row++);
+			cout << tmp;
+		}
+	}
+	ConsoleGraphics::getInstance().color(0);
+}
+
+void BackgroundController::beginInterface() {
+	drawPicFromPath("Patterns/House.txt", 50, 10, FG_WHITE);
+	drawPicFromPath("Patterns/House.txt", 150, 20, FG_WHITE);
+	drawPicFromPath("Patterns/Name.txt", 80, 10, FG_WHITE);
+	drawPicFromPath("Patterns/Pattern 5.txt", 0, 33, FG_WHITE);
+}
+
+void BackgroundController::simpleBackground(int type) {
+	int color;
+	if (type == 1) {
+		color = FG_BLUE | FG_GREEN | FG_I;
+	}
+	else {
+		color = FG_GREEN;
+	}
+	drawPicFromPath("Patterns/House.txt", 50, 10, color);
+	drawPicFromPath("Patterns/House.txt", 150, 20, color);
+	drawPicFromPath("Patterns/Trees.txt", 140, 0, color);
+	drawPicFromPath("Patterns/Trees.txt", 0, 25, color);
+}
+
+void BackgroundController::loginInterface(int type) {
+	int color;
+	if (type == 1) {
+		color = FG_BLUE | FG_GREEN | FG_I;
+	}
+	else {
+		color = FG_GREEN | FG_I;
+	}
+	drawPicFromPath("Patterns/Login.txt", 90, 10, color);
+	simpleBackground(type);
+}
